71e1875707bf0813221058943179c4a0b67eea2e
Jon Hirsch <jonmh99@gmail.com>
2017-10-16 22:56:15 -0700

    add l-system rendering functions

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import '../styles/app.scss';
+import renderLSystem from './renderLSystem';
 
-const container = document.getElementById('lSystemContainer');
+document.addEventListener('DOMContentLoaded', initLSystem);
 
-ReactDOM.render(<App />, container);
+function initLSystem() {
+  const container = document.getElementById('lSystemContainer');
+  const controlsContainer = document.createElement('div');
+  const canvas = document.createElement('canvas');
+  canvas.width = 800;
+  canvas.height = 480;
+  const context = canvas.getContext('2d');
+  container.appendChild(canvas);
+  container.appendChild(controlsContainer);
+
+  ReactDOM.render(<App />, controlsContainer);
+
+  const symbols = [];
+  symbols.push({ name: 'x', operation: 'move', amount: 2, productionRules: 'f-[[x]+x]+f[+fx]-x' });
+  symbols.push({ name: 'f', operation: 'move', amount: 2, productionRules: 'ff' });
+  symbols.push({ name: '+', operation: 'rotate', amount: 25, productionRules: '+' });
+  symbols.push({ name: '-', operation: 'rotate', amount: -25, productionRules: '-' });
+  symbols.push({ name: '[', operation: 'startBranch', amount: '', productionRules: '[' });
+  symbols.push({ name: ']', operation: 'endBranch', amount: '', productionRules: ']' });
+
+  renderLSystem(symbols, 'x', 6, 400, 400, -0.5 * Math.PI, context);
+}
---
+++ src/js/renderLSystem.js
@@ -0,0 +1,71 @@
+export default function renderLSystem(
+  symbols,
+  axiom,
+  iterations,
+  startX,
+  startY,
+  startAngle,
+  context
+) {
+  const sequence = generateSequence(symbols, axiom, iterations);
+  drawSequence(sequence, symbols, startX, startY, startAngle, context);
+}
+
+function generateSequence(symbols, axiom, iterations) {
+  let sequence = axiom;
+  const ruleMap = symbols.reduce((map, symbol) => {
+    map[symbol.name] = symbol.productionRules;
+    return map;
+  }, {});
+
+  for (let i = 0; i < iterations; i++) {
+    sequence = sequence.split('').reduce((acc, symbolName) => {
+      if (acc.length > 100000) return acc; //prevents the app from getting hung up on a crazy number of operations
+      return acc + ruleMap[symbolName];
+    }, '');
+  }
+  return sequence.slice(0, 100000);
+}
+
+function drawSequence(sequence, symbols, startX, startY, startAngle, context) {
+  let currentX = startX;
+  let currentY = startY;
+  let currentAngle = startAngle;
+  let branches = [];
+  context.strokeStyle = '#fff';
+  context.fillStyle = '#202020';
+  context.beginPath();
+  context.moveTo(currentX, currentY);
+  context.fillRect(0, 0, 800, 480);
+
+  const symbolMap = symbols.reduce((map, symbol) => {
+    map[symbol.name] = symbol;
+    return map;
+  }, {});
+
+  sequence.split('').forEach(symbolName => {
+    let symbol = symbolMap[symbolName];
+
+    switch (symbol.operation) {
+      case 'move':
+        currentX += Math.cos(currentAngle) * symbol.amount;
+        currentY += Math.sin(currentAngle) * symbol.amount;
+        context.lineTo(currentX, currentY);
+        break;
+      case 'rotate':
+        currentAngle += symbol.amount * Math.PI / 180;
+        break;
+      case 'startBranch':
+        branches.push({ x: currentX, y: currentY, angle: currentAngle });
+        break;
+      case 'endBranch':
+        const branch = branches.pop();
+        currentX = branch.x;
+        currentY = branch.y;
+        currentAngle = branch.angle;
+        context.moveTo(currentX, currentY);
+        break;
+    }
+  });
+  context.stroke();
+}
